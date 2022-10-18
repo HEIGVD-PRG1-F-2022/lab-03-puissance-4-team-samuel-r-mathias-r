4cadbfe7ff698f843487d00da5f5e2db6ec87ab7
mathias.renoult <mathias.renoult@protonmail.ch>
2022-10-18 16:24:47 +0200

    Add playMove and hasWon

--- game.cpp
+++ game.cpp
@@ -1,21 +1,29 @@
 #include "game.h"
-#include <vector>
 
 using namespace std;
 
-enum CaseContent { EMPTY,
-                   P1,
-                   P2 };
+gameResult hasWon(vector<vector<caseContent>> &board) {
 
+}
 
+int playMove(int moveIndex) {
+    int playerIndex = (moveIndex % 2) + 1;
+    int column = -1;
+    do {
+        cout << "Player " << playerIndex << " turn: ";
+        cin >> column;
+    } while (column < 1 || column > 7);
+
+    return column;
+}
 
 int playGame(std::vector<int> tableSize, bool isAIPlaying) {
     //Define our table
-	vector<vector<CaseContent>> board(tableSize[0], vector<CaseContent>(tableSize[1], EMPTY));
-
-	//Game loop
-	while(!hasWon()) {
-
-	}
-
+    vector<vector<caseContent>> board(tableSize[0], vector<caseContent>(tableSize[1], EMPTY));
+    int moveIndex;
+    //Game loop
+    while (hasWon(board) == NOT_FINISHED) {
+        playMove(moveIndex);
+        ++moveIndex;
+    }
 }
--- game.h
+++ game.h
@@ -1,3 +1,14 @@
+#include <iostream>
 #include <vector>
+using namespace std;
+enum gameResult {
+    NOT_FINISHED, DRAW, PLAYER1, PLAYER2
+};
+enum caseContent {
+    EMPTY,
+    P1,
+    P2
+};
 
+gameResult hasWon(vector<vector<caseContent>> &board);
 int playGame(std::vector<int> tableSize, bool isAIPlaying);
