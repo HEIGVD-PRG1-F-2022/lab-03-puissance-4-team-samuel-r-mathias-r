da6f5db0ed6e72bfe7a1f47a510a48cd8930ec48
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-10-18 16:01:56 +0200

    Indent main.cpp

--- main.cpp
+++ main.cpp
@@ -4,45 +4,57 @@
  * Created : 10.18.22
  */
 
-#include <iostream>
 #include <iomanip>
+#include <iostream>
 
 using namespace std;
 
-void changeSettings()
-{
+void changeSettings() {
     system("cls");
-    cout << "What settings would you like to change?" << endl << "1. AI mode" << endl << "2. Board size" << endl << "3. Colors" << endl;
+    cout << "What settings would you like to change?" << endl
+         << "1. AI mode" << endl
+         << "2. Board size" << endl
+         << "3. Colors" << endl;
     int choice;
     cin >> choice;
-    switch(choice)
-    {
+    switch (choice) {
         case 1:
             system("cls");
-            cout << "AI mode :" << endl << "1. Smart" << endl << "2. Random" << endl;
+            cout << "AI mode :" << endl
+                 << "1. Smart" << endl
+                 << "2. Random" << endl;
             cin >> choice;
             choice == 1 ? 1 : 2;
-            if(choice > 2 || choice < 1) cout << "ERREUR!";
+            if (choice > 2 || choice < 1) cout << "ERREUR!";
             break;
-        case 2: playerVSAI(); break;
-        case 3: break;
+        case 2:
+            playerVSAI();
+            break;
+        case 3:
+            break;
     }
 }
 
-void gameMenu()
-{
-    cout << "Welcome to *Connect Four*!" << endl << "1. Player vs AI" << endl << "2. Player vs Player" << endl << "3. Settings" << endl;
+void gameMenu() {
+    cout << "Welcome to *Connect Four*!" << endl
+         << "1. Player vs AI" << endl
+         << "2. Player vs Player" << endl
+         << "3. Settings" << endl;
     int choice;
     cin >> choice;
-    switch(choice)
-    {
-        case 1: playerVSplayer(); break;
-        case 2: playerVSAI(); break;
-        case 3: break;
+    switch (choice) {
+        case 1:
+            playerVSplayer();
+            break;
+        case 2:
+            playerVSAI();
+            break;
+        case 3:
+            break;
     }
 }
 
-int main(){
+int main() {
 
     return 0;
 }
