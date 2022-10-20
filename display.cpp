c87d4ea2406573410fffbbeb30475ea20bf2d975
Samuel Roland <47849646+samuelroland@users.noreply.github.com>
2022-10-20 14:33:38 +0200

    Enhance display with numbers and arrow down

--- display.cpp
+++ display.cpp
@@ -2,19 +2,31 @@
 //Code taken from https://stackoverflow.com/a/15481700
 #ifdef _WIN32
 #define CLEARCMD "cls"
-#else //In any other OS
+#else//In any other OS
 #define CLEARCMD "clear"
 #endif
 
 using namespace std;
 
 void displayBoard(const std::vector<std::vector<caseContent>> &board) {
+    cout << " ";
+    for (int i = 1; i <= board[0].size(); i++) {
+        cout << " " << i << " " << (board[0].size() == i ? "" : " ");
+    }
+    cout << " " << endl;
+    for (int j = 0; j < board[0].size(); j++) {
+        cout << "  \u25BC ";
+    }
+    cout << endl
+         << endl;
+
     for (int line = 0; line < board.size(); line++) {
         for (int col = 0; col < board[0].size(); col++) {
-            cout << "|" << (board[line][col] == EMPTY ? " " : (board[line][col] == P1 ? "X" : "O"));
+            cout << "|" << (board[line][col] == EMPTY ? "   " : (board[line][col] == P1 ? " X " : " O "));
         }
         cout << "|" << endl;
     }
+    cout << endl;
 }
 
 void clearScreen() {
