0a8611fd8495bd22db31de2f71e2c57c0e053299
mathias.renoult <mathias.renoult@protonmail.ch>
2022-10-22 01:55:45 +0200

    Fixed indentation & logic issues in game.cpp

--- game.cpp
+++ game.cpp
@@ -20,40 +20,75 @@ gameResult hasWon(const vector<vector<caseContent>> &board, const std::vector<in
     int y = 1;
     int score = 0;
     caseContent checkedPlayer = board[lastPlayedCell[0]][lastPlayedCell[1]];
-    for(int i = 0; i <7;i++)
-    {
-        switch(i){
-            case 0: x = i; y = i; break;
-            case 1: x = 0; y = i; break;
-            case 2: x = i*-1; y = i*-1; break;
-            case 3: x = i*-1; y = 0; break;
-            case 4: x = i*-1; y = i*-1; break;
-            case 5: x = 0; y = i*-1; break;
-            case 6: x = i; y = i; break;
-            default: cout << "ERROR" << endl;
+    for (int i = 0; i < 7; i++) {
+        switch (i) {
+            case 0:
+                x = i;
+                y = i;
+                break;
+            case 1:
+                x = 0;
+                y = i;
+                break;
+            case 2:
+                x = i * -1;
+                y = i * -1;
+                break;
+            case 3:
+                x = i * -1;
+                y = 0;
+                break;
+            case 4:
+                x = i * -1;
+                y = i * -1;
+                break;
+            case 5:
+                x = 0;
+                y = i * -1;
+                break;
+            case 6:
+                x = i;
+                y = i;
+                break;
+            default:
+                cout << "ERROR" << endl;
         }
-        if((lastPlayedCell[0]+x >= board.size() ||
-            lastPlayedCell[1]+y >= board[0].size() ||
-            lastPlayedCell[0]+x < 0 ||
-            lastPlayedCell[1]+y < 0) &&
-            board[lastPlayedCell[0]+x][lastPlayedCell[1]+y] == checkedPlayer){
-                score++;
-        }
-        else{
-            switch(i){
-                case 0: NE = score; break;
-                case 1: E  = score; break;
-                case 2: SE = score; break;
-                case 3: S  = score; break;
-                case 4: SW = score; break;
-                case 5: W  = score; break;
-                case 6: NW = score; break;
-                default: cout << "ERROR" << endl;
+        if ((lastPlayedCell[0] + x >= board.size() ||
+             lastPlayedCell[1] + y >= board[0].size() ||
+             lastPlayedCell[0] + x < 0 ||
+             lastPlayedCell[1] + y < 0) &&
+            board[lastPlayedCell[0] + x][lastPlayedCell[1] + y] == checkedPlayer) {
+            score++;
+        } else {
+            switch (i) {
+                case 0:
+                    NE = score;
+                    break;
+                case 1:
+                    E = score;
+                    break;
+                case 2:
+                    SE = score;
+                    break;
+                case 3:
+                    S = score;
+                    break;
+                case 4:
+                    SW = score;
+                    break;
+                case 5:
+                    W = score;
+                    break;
+                case 6:
+                    NW = score;
+                    break;
+                default:
+                    cout << "ERROR" << endl;
             }
             score = 0;
         }
     }
-    if(NE + SW >= 3 || S >= 3 || E + W >= 3 || NW + SE >= 3) return gameResult(checkedPlayer + 1);
+    if (NE + SW >= 3 || S >= 3 || E + W >= 3 || NW + SE >= 3) return gameResult(checkedPlayer + 1);
 
     return NOT_FINISHED;
 }
@@ -63,7 +98,7 @@ bool isMoveValid(int column, vector<vector<caseContent>> &board) {
 }
 
 vector<int> playMove(int moveIndex, vector<vector<caseContent>> &board) {
-    if(moveIndex == board.size() * board[0].size()) return DRAW;
+    if (moveIndex == board.size() * board[0].size()); //Draw
     int playerIndex = (moveIndex % 2) + 1;
     int column;
     do {
